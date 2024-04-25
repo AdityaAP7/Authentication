99fe38a7bacf8c51541db5eb2cf0dfe490b2b361
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-04-25 23:24:50 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -63,6 +63,22 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 6,
+      username: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[a-zA-Z0-9]{6,12}$/,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
   },
   },
 });
