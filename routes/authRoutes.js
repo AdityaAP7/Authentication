ac26185f784c76542f16c3f496f5867f53926bb5
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-04-25 23:24:04 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const { register, login } = require('../controllers/AuthController');
+const { validateUser } = require('../middlewares/validationMiddleware');
+
+// User registration route
+router.post('/register', validateUser, register);
+
+// User login route
+router.post('/login', login);
+
+module.exports = router;
 
